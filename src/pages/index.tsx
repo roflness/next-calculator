e827119aba74ac14efb98ce808924413e61aee65
roflness <rcreyes.951@gmail.com>
2024-07-18 12:56:52 -0700

    fixed styles link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import Head from 'next/head';
+
 
 
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return (
+      <>
+        <Head>
+          <link rel="stylesheet" href="/styles.css" />
+        </Head>
+        <h1>Hello, Next.js!</h1>
+      </>
+    )
   }
