e097ffc5f863a064c50066e1678edb34db5a98ad
roflness <rcreyes.951@gmail.com>
2024-07-18 16:42:50 -0700

    changed env variables

--- src/app/api.js
+++ src/app/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000';
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // Fetch greeting message from the backend
 export const fetchGreeting = async () => {
