2a07d2cf0e7d63f973c033aa824dc92fa8e6f906
roflness <rcreyes.951@gmail.com>
2024-07-18 16:25:13 -0700

    fixed rest of ghg results

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -83,12 +83,12 @@ const Results = () => {
             <br/>
             <h2 className="fs-second-title">GHG reductions as difference between gasoline and electric GHGs</h2>
             <p><span className="fs-title">Miles Driven: </span>{results?.ghg_reduction_result?.['Miles Driven'] ?? 'N/A'}</p>
-            <p><span className="fs-title">Gasoline Used (gallons): </span>{safeToFixed(results.ghg_reduction_result['Gasoline Used (gallons)'])}</p>
-            <p><span className="fs-title">Electricity Used (kW): </span>{safeToFixed(results.ghg_reduction_result['Electricity Used (kWh)'])}</p>
+            <p><span className="fs-title">Gasoline Used (gallons): </span>{results?.ghg_reduction_result?.['Gasoline Used (gallons)'] ?? 'N/A'}</p>
+            <p><span className="fs-title">Electricity Used (kW): </span>{results?.ghg_reduction_result?.['Electricity Used (kWh)'] ?? 'N/A'}</p>
             <br/>
-            <p><span className="fs-title">Gasoline Fuel Usage GHGs (MT CO2): </span>{safeToFixed(results.ghg_reduction_result['Gasoline Fuel Usage GHGs (MT CO2)'])}</p>
-            <p><span className="fs-title">Elec Fuel Usage GHGs (MT CO2): </span>{safeToFixed(results.ghg_reduction_result['Electric Fuel Usage GHGs (MT CO2)'])}</p>
-            <p><span className="fs-title">Net GHG Reductions (MT CO2): </span>{safeToFixed(results.ghg_reduction_result['Net GHG Reductions (MT CO2)'])}</p>
+            <p><span className="fs-title">Gasoline Fuel Usage GHGs (MT CO2): </span>{results?.ghg_reduction_result?.['Gasoline Fuel Usage GHGs (MT CO2)'] ?? 'N/A'}</p>
+            <p><span className="fs-title">Elec Fuel Usage GHGs (MT CO2): </span>{results?.ghg_reduction_result?.['Electric Fuel Usage GHGs (MT CO2)'] ?? 'N/A'}</p>
+            <p><span className="fs-title">Net GHG Reductions (MT CO2): </span>{results?.ghg_reduction_result?.['Net GHG Reductions (MT CO2)'] ?? 'N/A'}</p>
             <br/>
             <p>
                 <Link href="/" className="secondary-button">Calculate Again</Link>
