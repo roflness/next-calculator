284b5f45e4530683d2649d8972ad1e91528d54d2
roflness <rcreyes.951@gmail.com>
2024-07-19 13:51:27 -0700

    fixed typo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import theme from '../theme'';
+import theme from '../theme';
 import createEmotionCache from '../createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
