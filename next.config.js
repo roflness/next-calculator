d98a91475fcec8dfd7c5bdc864ccbccedfe01e7b
roflness <rcreyes.951@gmail.com>
2024-07-19 11:15:49 -0700

    changed config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 // next.config.js
 module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
   async rewrites() {
     const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000';
     return [
